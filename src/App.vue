f9ec2663a52e2344f200818549180ae62e3a1b66
Artem <darkoto12@gmail.com>
2023-07-05 23:53:49 +0300

    add fonts, iconfonts, mixins, variable, config. Create header

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <div class="content">
+      <the-header></the-header>
+      <router-view class="page" />
+      <the-footer></the-footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store/store'
+// import axios from './plugins/axios'
+import "@/assets/style/main.scss"
+
+
+
+const app = createApp(App)
+
+app.use(router)
+app.use(store)
+// app.use(axios)
+
+app.mount('#app')
