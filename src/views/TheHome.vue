4b44f23a6672fa5c68872cb27ffb3770a1b900c3
Artem <darkoto12@gmail.com>
2023-07-20 21:35:49 +0300

    add navigation genres

--- src/views/TheHome.vue
+++ src/views/TheHome.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="home">
     <banner />
+    <navigation v-model="navValue" />
   </div>
 </template>
 
 <script>
 import banner from "@/components/TheBanner.vue";
+import navigation from "@/components/TheNavigation.vue";
 
 export default {
   name: "TheHome",
   components: {
     banner,
+    navigation
   },
+  data(){
+    return{
+      navValue: "Приключения"
+    }
+  }
 };
 </script>
